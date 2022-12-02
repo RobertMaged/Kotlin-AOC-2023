94a4c6b25fa16c7b5e0efda828f6fa1de8000375
Robert <rmsr888@gmail.com>
2022-12-02 12:54:29 +0200

    Day01 improved version

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,47 +1,54 @@
+fun part1(input: List<String>): Int {
+
+    var maxRecordedCalorie = 0
+
+    var currentElvesCalorieSum = 0
+
+    for (line in input) {
+
+        when (val calorie = line.toIntOrNull()) {
+            null -> currentElvesCalorieSum = 0
+
+            else -> currentElvesCalorieSum += calorie
+        }
+
+        maxRecordedCalorie = maxOf(maxRecordedCalorie, currentElvesCalorieSum)
+    }
+
+    return maxRecordedCalorie
+}
+
+fun part2(input: List<String>): Int = buildList {
+    var currentElvesCalorieSum = 0
+
+    for (lineIndex in input.indices) {
+
+        val calorie = input[lineIndex].toIntOrNull()
+
+        when {
+            calorie == null -> {
+                add(currentElvesCalorieSum)
+                currentElvesCalorieSum = 0
+            }
+
+            // to not miss adding last number in file
+            lineIndex == input.lastIndex -> add(currentElvesCalorieSum + calorie)
+
+            else -> currentElvesCalorieSum += calorie
+        }
+
+    }
+
+
+}.sortedDescending()
+    .take(3)
+    .sum()
+
 fun main() {
-    /*
-        split every group chunk, using empty line
 
-        sum each chunk
-     */
-
-    fun part1(input: List<String>): Int {
-
-        var max = 0
-        var oneElvesSum = 0
-        for (line in input) {
-            val calorie = line.toIntOrNull()
-
-            if (calorie == null)
-                max = maxOf(max, oneElvesSum).also { oneElvesSum = 0 }
-            else
-                oneElvesSum += calorie
-        }
-
-        return max
-    }
-
-    fun part2(input: List<String>): Int = buildList {
-        var oneElvesSum = 0
-
-        for (line in input) {
-
-            val calorie = line.toIntOrNull()
-
-            if (calorie == null) {
-                add(oneElvesSum)
-                oneElvesSum = 0
-            } else
-                oneElvesSum += calorie
-        }
-
-    }
-        .sortedDescending()
-        .take(3)
-        .sum()
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test").plus("\n")
+    val testInput = readInput("Day01_test")//.plus("\n")
     check(part1(testInput).also(::println) == 24000)
     check(part2(testInput).also(::println) == 45000)
 
