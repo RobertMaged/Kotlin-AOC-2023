984995a208bb4aa70567aea22c3ea0e32e85546a
RobertMaged <rmsr888@gmail.com>
2022-12-04 08:49:46 +0200

    Day04 improved version

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,27 +1,23 @@
 fun main() {
-    fun String.splitTwoPairs() = this.split(',').map { onePair ->
-        val (start, end) = onePair.split('-').map { it.toInt() }
-        start..end
+    fun String.splitElves() = this.split(',').map { elf ->
+        val (startSection, endSection) = elf.split('-').map { it.toInt() }
+        startSection..endSection
     }
 
-    fun part1(input: List<String>): Int = input.count { line ->
+    fun part1(input: List<String>): Int = input.count { assignmentPair ->
 
-        val (a, b) = line.splitTwoPairs()
+        val (firstElf, secondElf) = assignmentPair.splitElves()
 
-        // alternative
-//        a.first <= b.first && a.last >= b.last -> count++
-//        b.first <= a.first && b.last >= a.last -> count++
-
-        return@count when ((a intersect b).size) {
-            a.count(), b.count() -> true
+        return@count when ((firstElf intersect secondElf).size) {
+            firstElf.count(), secondElf.count() -> true
             else -> false
         }
     }
 
     fun part2(input: List<String>): Int = input.count { line ->
-        val (a, b) = line.splitTwoPairs()
+        val (firstElf, secondElf) = line.splitElves()
 
-        return@count (a intersect b).any()
+        return@count (firstElf intersect secondElf).any()
     }
 
 
@@ -33,3 +29,7 @@ fun main() {
     println(part1(input))
     println(part2(input))
 }
+
+// part one alternative
+//        firstElf.first <= secondElf.first && firstElf.last >= secondElf.last -> count++
+//        secondElf.first <= firstElf.first && secondElf.last >= firstElf.last -> count++
