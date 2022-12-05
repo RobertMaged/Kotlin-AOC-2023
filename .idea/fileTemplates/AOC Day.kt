e7ac84d837b549c2fa236bf0a56b05f286f293c6
RobertMaged <rmsr888@gmail.com>
2022-12-05 04:18:30 +0200

    Create new AOC Day template - wasting some time.

--- .idea/fileTemplates/AOC Day.kt
+++ .idea/fileTemplates/AOC Day.kt
@@ -10,7 +10,8 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day${DAY_NUMBER_FULL}_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput).also(::println) == 1)
+    //check(part2(testInput).also(::println) == 1)
 
     val input = readInput("Day${DAY_NUMBER_FULL}")
     println(part1(input))
