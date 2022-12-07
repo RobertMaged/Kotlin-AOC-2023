d1796b6f1eae2aaa215b0552176deddb4aae6535
RobertMaged <rmsr888@gmail.com>
2022-12-07 11:10:26 +0200

    Day07 improved version

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,90 +1,59 @@
-data class Dir(val name  : String, val child: List<Dir>?, val size: Int)
-
-
-
-
 fun main() {
-    fun ArrayDeque<String>.prefixes(): List<String> = runningReduce { acc, s -> acc + s }
+    fun ArrayDeque<String>.toPWDs(): List<String> = runningReduce { pwdAcc, curDir -> pwdAcc + curDir }
+
+    fun Map<String, Int>.getDeviceFreeSpace(): Int = ( 70_000_000 - this.getValue("/") )
+
+    fun List<String>.dirsSize(): Map<String, Int> {
+        val filesStack = ArrayDeque(listOf("/"))
+        val sizes = mutableMapOf<String, Int>()
+
+        for (line in this) when {
+            line.startsWith("\$ cd /") -> run { filesStack.clear(); filesStack.addLast("/") }
+
+            line.startsWith("\$ cd ..") -> filesStack.removeLast()
+
+            line.startsWith("\$ cd") -> filesStack.addLast("${line.substringAfter("cd ")}/")
+
+            line.startsWith("\$ ls") || line.startsWith("dir") -> Unit // no need to track files themselves
+
+            // accumulate dirs hierarchy sizes
+            line.first().isDigit() -> filesStack.toPWDs().forEach { pwd ->
+                val fileSize = line.split(" ").first().toInt()
+                sizes[pwd] = sizes.getOrDefault(pwd, 0) + fileSize
+            }
+        }
+        return sizes.toMap()
+    }
+
     fun part1(input: List<String>): Int {
-val filesStack = ArrayDeque<String>(listOf("/"))
-val sizes = mutableMapOf<String, Long>()
-//        val filesStack = ArrayDeque<String>(listOf("/"))
-//        val dirMap = mutableMapOf<String, Set<String>>("/" to emptySet<String>())
-//        for (line in input){
-/*
-            when{
-                line.equals("$ cd /") -> run { while (filesStack.last() != "/") filesStack.removeLast() }
-                line.contains("cd")-> { line.substringAfter("cd ").also{dirMap[filesStack.last()] = (dirMap[filesStack.last()] ?: emptySet()) + setOf(it)}.also(filesStack::addLast)}
-                line.contains("cd ..") -> filesStack.removeLast()
-                line.contains("ls") ->
+        val filesStack = ArrayDeque(listOf("/"))
+        val sizes = mutableMapOf<String, Int>()
+
+        for (line in input) when {
+            line.startsWith("\$ cd /") -> run { filesStack.clear(); filesStack.addLast("/") }
+
+            line.startsWith("\$ cd ..") -> filesStack.removeLast()
+
+            line.startsWith("\$ cd") -> filesStack.addLast("${line.substringAfter("cd ")}/")
+
+            line.startsWith("\$ ls") || line.startsWith("dir") -> Unit // no need to track files themselves
+
+            // accumulate dirs hierarchy sizes
+            line.first().isDigit() -> filesStack.toPWDs().forEach { pwd ->
+                val fileSize = line.split(" ").first().toInt()
+                sizes[pwd] = sizes.getOrDefault(pwd, 0) + fileSize
             }
-*/
-//        }
-        for (line in input){
-
-            when{
-                line.startsWith("\$ cd /") -> run {filesStack.clear(); filesStack.addLast("/") }
-                line .startsWith ("\$ cd ..") -> {
-                    filesStack.removeLast();
-                }
-                line.startsWith("\$ cd")-> { line.substringAfter("cd ").also { filesStack.addLast("$it/")}}//; filesStack.addLast("/")}}
-                line.startsWith("\$ ls") || line.startsWith("dir") -> Unit
-                line.first().isDigit() -> line.split(" ").let {
-                for (d in filesStack.prefixes()) {
-//                    sizes[d + it[1]] = sizes.getOrDefault(d + it[1], 0) + it[0].toLong()
-                    sizes[d] = sizes.getOrDefault(d, 0) + it[0].toLong()
-                }
-                }
-
-            }
-
         }
 
-        return sizes.values.filter { it <= 100000L }.sum()
-            .toInt()
+
+        return sizes.values.filter { it <= 100_000 }.sum()
     }
 
     fun part2(input: List<String>): Int {
+        val sizes = input.dirsSize()
 
-        val filesStack = ArrayDeque<String>(listOf("/"))
-        val sizes = mutableMapOf<String, Long>()
-//        val filesStack = ArrayDeque<String>(listOf("/"))
-//        val dirMap = mutableMapOf<String, Set<String>>("/" to emptySet<String>())
-//        for (line in input){
-        /*
-                    when{
-                        line.equals("$ cd /") -> run { while (filesStack.last() != "/") filesStack.removeLast() }
-                        line.contains("cd")-> { line.substringAfter("cd ").also{dirMap[filesStack.last()] = (dirMap[filesStack.last()] ?: emptySet()) + setOf(it)}.also(filesStack::addLast)}
-                        line.contains("cd ..") -> filesStack.removeLast()
-                        line.contains("ls") ->
-                    }
-        */
-//        }
-        for (line in input){
-
-            when{
-                line.startsWith("\$ cd /") -> run {filesStack.clear(); filesStack.addLast("/") }
-                line .startsWith ("\$ cd ..") -> {
-                    filesStack.removeLast();
-                }
-                line.startsWith("\$ cd")-> { line.substringAfter("cd ").also { filesStack.addLast("$it/")}}//; filesStack.addLast("/")}}
-                line.startsWith("\$ ls") || line.startsWith("dir") -> Unit
-                line.first().isDigit() -> line.split(" ").let {
-                    for (d in filesStack.prefixes()) {
-//                    sizes[d + it[1]] = sizes.getOrDefault(d + it[1], 0) + it[0].toLong()
-                        sizes[d] = sizes.getOrDefault(d, 0) + it[0].toLong()
-                    }
-                }
-
-            }
-
-        }
-
-        return sizes.values.filter{ it >= 30_000_000L - (70_000_000L - sizes[filesStack.first()]!!) }
-            .minOf {
-                it
-            }
-            ?.toInt()!!
+        val neededFreeSpace = 30_000_000L - sizes.getDeviceFreeSpace()
+        return sizes.values.filter { it >= neededFreeSpace }.min()
     }
 
     // test if implementation meets criteria from the description, like:
