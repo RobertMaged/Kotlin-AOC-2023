99f262c6377f2fdaad84d5a868c422958463d0e6
RobertMaged <rmsr888@gmail.com>
2022-12-10 09:56:02 +0200

    Day10 just working version

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,3 +1,5 @@
+import java.util.Collections
+
 fun main() {
     fun part1(input: List<String>): Int {
         var products = mutableListOf<Int>()
@@ -6,15 +8,15 @@ fun main() {
 
             if (ins == "noop") {
                 listOf(0)
-            }else{
+            } else {
 
                 val n = line.dropWhile { (it != '-' && it.isLetter()) || it == ' ' }
 
                 listOf(0, n.toInt())
             }
-        }.foldIndexed(1){index, acc, add ->
-            if (index+1 in listOf(20, 60,100 , 140, 180, 220))
-                products.add((index+1) * acc)
+        }.foldIndexed(1) { index, acc, add ->
+            if (index + 1 in listOf(20, 60, 100, 140, 180, 220))
+                products.add((index + 1) * acc)
 
             acc + add
         }
@@ -24,15 +26,54 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
+        val spiritPosititon = List(40) { if (it < 3) '#' else '.' }.toMutableList()
+
+/*        for (line in input){
+            val ins = line.takeWhile { it.isLetter() }
+            var curr = 0
+            if (ins == "noop") {
+//                listOf(0)
+            } else {
+
+                val n = line.dropWhile { (it != '-' && it.isLetter()) || it == ' ' }
+
+                curr = n.toInt()
+            }
+        }*/
+
+        input.flatMap { line ->
+            val ins = line.takeWhile { it.isLetter() }
+
+            if (ins == "noop") {
+                listOf(0)
+            } else {
+
+                val n = line.dropWhile { (it != '-' && it.isLetter()) || it == ' ' }
+
+                listOf(0, n.toInt())
+            }
+        }.foldIndexed(1) { index, acc, add ->
+            require(spiritPosititon.size == 40)
+
+            val pixel = (index ) % 40
+            print(spiritPosititon[pixel])
+            if ((index + 1) % 40 == 0) {
+                println()
+            }
+            Collections.rotate(spiritPosititon, add )
+//println(spiritPosititon.joinToString(separator = ""))
+            acc + add
+        }
         return input.size
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day10_test")
-    check(part1(testInput).also(::println) == 13140)
-    //check(part2(testInput).also(::println) == 1)
+//    check(part1(testInput).also(::println) == 13140)
+//    check(part2(testInput).also(::println) == 1)
 
     val input = readInput("Day10")
-    println(part1(input))
-    println(part2(input))
+//    println(part1(input))
+//    println(part2(input))
+    part2(input)
 }
