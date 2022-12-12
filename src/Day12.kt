569b1e74e0a2a0f8858918f7e347a772d5251318
RobertMaged <rmsr888@gmail.com>
2022-12-12 17:18:08 +0200

    Day12 improved version

--- src/Day12.kt
+++ src/Day12.kt
@@ -2,225 +2,152 @@ private typealias Ver = Pair<Int, Int>
 
 private val Ver.y get() = first
 private val Ver.x get() = second
-fun main(args: Array<String>) {
 
+private fun Array<CharArray>.findEdgesIndices(): Pair<Ver, Ver> {
+    val c2D = this@findEdgesIndices
+    val edgs = Array(2) { -1 to -1 }
+    var requiredEdgesFound = 0
+    edgesFinder@ for (i in c2D.indices) {
+        for (j in c2D[0].indices) {
+            if (c2D[i][j] != 'S' && c2D[i][j] != 'E') continue
 
-    fun part1(input: List<String>): Int {
-
-        val c2D = input.map { it.toCharArray() }.toTypedArray()
-
-        val sIndex = listOf(0 to 0, 20 to 0).first {
-            c2D[it.y][it.x] == 'S'
-        }
-        val eIndex = listOf(2 to 5, 20 to 120).first {
-            c2D[it.y][it.x] == 'E'
-        }
-        c2D[sIndex.y][sIndex.x] = 'a'
-        c2D[eIndex.y][eIndex.x] = 'z'
-
-        val graph = buildMap<Ver, Set<Ver>> {
-            for (i in c2D.indices)
-                for (j in c2D[0].indices) {
-                    put(i to j, listOf(
-                        i - 1 to j, //top
-                        i to j - 1, //left
-                        i to j + 1, //right
-                        i + 1 to j,//bottom
-                    )
-                        .filter { it.y in c2D.indices && it.x in c2D[0].indices }
-                        .filter { c2D[it.y][it.x] - c2D[i][j] < 2 }
-                        .toSet()
-                    )
-                }
-        }
-
-
-        val queue = ArrayDeque(listOf(sIndex))
-        val searched = mutableSetOf<Ver>()
-        val parents = mutableMapOf<Ver, Ver>()
-        while (queue.any()) {
-            val currPlaceVer = queue.removeFirst()
-            if (currPlaceVer in searched) continue
-            searched += currPlaceVer
-
-            val adj = graph[currPlaceVer]!!
-            adj.forEach {
-                if (it !in searched)
-                    parents[it] = currPlaceVer
-            }
-
-            if (eIndex in adj) {
-                println("found by $currPlaceVer")
-                val path = mutableSetOf<Ver>(currPlaceVer)
-                var last: Ver? = currPlaceVer
-                while (last != null && last != eIndex) {
-                    path.add(last)
-                    val n = parents[last]
-                    last = n
-                }
-
-                return path.size
+            if (c2D[i][j] == 'S') {
+                c2D[i][j] = 'a'
+                edgs[0] = i to j
             } else {
-                adj.forEach(queue::addLast)
+                c2D[i][j] = 'z'
+                edgs[1] = i to j
             }
-        }
 
-        return error("not accepted")
+            requiredEdgesFound++
+            if (requiredEdgesFound == 2) break@edgesFinder
+        }
     }
-
-    fun part2Dummy(input: List<String>): Int {
-        val map = mutableMapOf('S' to 'b')
-
-        val c2D = input.map { it.toCharArray() }.toTypedArray()
-
-        val SInd = listOf(0 to 0, 20 to 0).first {
-            c2D[it.y][it.x] == 'S'
-        }
-        val eIndex = listOf(2 to 5, 20 to 120).first {
-            c2D[it.y][it.x] == 'E'
-        }
-        c2D[SInd.y][SInd.x] = 'a'
-        c2D[eIndex.y][eIndex.x] = 'z'
-
-        val graph = buildMap<Ver, Set<Ver>> {
-            for (i in c2D.indices)
-                for (j in c2D[0].indices) {
-//                    if (c2D[i][j] == 'E') continue
-                    put(i to j, listOf(
-                        i - 1 to j, //top
-                        i to j - 1, //left
-                        i to j + 1, //right
-                        i + 1 to j,//bottom
-                    )
-                        .filter { it.y in c2D.indices && it.x in c2D[0].indices }
-//                        .filter { c2D[it.y][it.x] in listOf('E', 'S') || c2D[it.y][it.x] - c2D[i][j] < 2 }
-                        .filter { c2D[it.y][it.x] - c2D[i][j] < 2 }
-                        .toSet()
-                    )
-                }
-        }
-
-        val allAs = graph.keys.filter { c2D[it.y][it.x] == 'a' }
-        val sizes = mutableListOf<Int>()
-
-        for (sIndex in allAs) {
-
-            val queue = ArrayDeque(listOf(sIndex))
-            val searched = mutableSetOf<Ver>()
-            val parents = mutableMapOf<Ver, Ver>()
-            while (queue.any()) {
-                val currPlaceVer = queue.removeFirst()
-                if (currPlaceVer in searched) continue
-                searched += currPlaceVer
-
-                val adj = graph[currPlaceVer]!!
-                adj.forEach {
-                    if (it !in searched)
-                        parents[it] = currPlaceVer
-                }
-
-                if (eIndex in adj) {
-                    val path = mutableSetOf<Ver>(currPlaceVer)
-                    var last2: Ver? = currPlaceVer
-                    while (last2 != null && last2 != eIndex) {
-                        path.add(last2)
-                        last2 = parents[last2]
-                    }
-
-                    sizes += path.size
-                } else {
-                    adj.forEach(queue::addLast)
-                }
-            }
-        }
-
-
-        return sizes.min()
-    }
-
-
-    val testInput = readInput("Day12_test")
-    val input = readInput("Day12")
-
-    check(part1(testInput).also(::println) == 31)
-    val p1 = part1(input)
-    println(p1)
-//    sendResult(answer = p1.toString(), part = 1, day = "12", year = 2022)
-
-    check(part2Dummy(testInput).also(::println) == 29)
-    val p2 = part2Dummy(input)
-    println(p2)
-    check(part2Dummy(input).also(::println) == 451)
-//    sendResult(answer = p2.toString(), part = 2, day = "12", year = 2022)
-
+    return edgs[0] to edgs[1]
 }
 
-private fun part2(input: List<String>): Int {
-    val map = mutableMapOf('S' to 'b')
-
-    val c2D = input.map { it.toCharArray() }.toTypedArray()
-
-    val sIndex = listOf(0 to 0, 20 to 0).first {
-        c2D[it.y][it.x] == 'S'
-    }
-    val eIndex = listOf(2 to 5, 20 to 120).first {
-        c2D[it.y][it.x] == 'E'
-    }
-    c2D[sIndex.y][sIndex.x] = 'a'
-    c2D[eIndex.y][eIndex.x] = 'z'
-
-    val graph = buildMap<Ver, Set<Ver>> {
-        for (i in c2D.indices)
-            for (j in c2D[0].indices) {
-//                    if (c2D[i][j] == 'E') continue
+private fun Array<CharArray>.drawHeightsGraph(onAdjacentFilter: (adjH: Ver, curH: Ver) -> Boolean) =
+    buildMap<Ver, Set<Ver>> {
+        val chars2D = this@drawHeightsGraph
+        for (i in chars2D.indices)
+            for (j in chars2D[0].indices) {
                 put(i to j, listOf(
                     i - 1 to j, //top
                     i to j - 1, //left
                     i to j + 1, //right
                     i + 1 to j,//bottom
                 )
-                    .filter { it.y in c2D.indices && it.x in c2D[0].indices }
-//                        .filter { c2D[it.y][it.x] in listOf('E', 'S') || c2D[it.y][it.x] - c2D[i][j] < 2 }
-//                        .filter { c2D[it.y][it.x] - c2D[i][j] < 2 }
-                    .filter { c2D[i][j] - c2D[it.y][it.x] > 1 }
+                    .filter { it.y in indices && it.x in chars2D[0].indices }
+                    .filter { onAdjacentFilter(it, i to j) }
                     .toSet()
                 )
             }
     }
 
+private fun <T : Ver> Map<T, T>.buildWalkedPath(endNode: T, endNodeParent: T) = buildSet<T> {
+    val parents = this@buildWalkedPath
 
-    val queue = ArrayDeque(listOf(eIndex))
+    add(endNodeParent)
 
-    val searched = mutableSetOf<Ver>()
-    val parents = mutableMapOf<Ver, Ver>()
-    while (queue.any()) {
-        val currPlaceVer = queue.removeFirst()
-        if (currPlaceVer in searched) continue
-        searched += currPlaceVer
-
-        val adj = graph[currPlaceVer]!!
-        adj.forEach {
-            if (it !in searched)
-                parents[it] = currPlaceVer
-        }
-
-        if ('a' in adj.map { c2D[it.y][it.x] }) {
-            val aIndex = adj.single { c2D[it.y][it.x] == 'a' }
-            println("found by $currPlaceVer")
-            val path = mutableSetOf<Ver>(currPlaceVer)
-            var last2: Ver? = currPlaceVer
-            while (last2 != null && last2 != aIndex) {
-                path.add(last2)
-                val n = parents[last2]
-                last2 = n
-            }
-            return path.size
-//                return 0
-        } else {
-            adj.forEach(queue::addLast)
-        }
+    var last: T? = endNodeParent
+    while (last != null && last != endNode) {
+        add(last)
+        last = parents[last]
     }
-
-    return error("not accepted")
 }
 
+
+fun main(args: Array<String>) {
+
+    fun part1(chars2D: Array<CharArray>): Int {
+
+        val (sIndex, eIndex) = chars2D.findEdgesIndices()
+
+        val graph = chars2D.drawHeightsGraph(onAdjacentFilter = { adjH, curH ->
+            chars2D[adjH.y][adjH.x] - chars2D[curH.y][curH.x] < 2
+        })
+
+        val queue = ArrayDeque(listOf(sIndex))
+        val searched = mutableSetOf<Ver>()
+        val parents = mutableMapOf<Ver, Ver>()
+
+        var lastAdjacentBeforeE: Ver? = null
+
+        while (queue.any()) {
+            val currPositionVer = queue.removeFirst()
+            if (currPositionVer in searched) continue
+            searched += currPositionVer
+
+            val adjacentVertices: Set<Ver> = graph[currPositionVer]!!
+
+            adjacentVertices
+                .filterNot { it in searched }
+                .forEach { parents[it] = currPositionVer }
+
+            if (eIndex in adjacentVertices) {
+                lastAdjacentBeforeE = currPositionVer
+                break
+            } else
+                adjacentVertices.forEach(queue::addLast)
+        }
+
+        return parents
+            .buildWalkedPath(endNode = eIndex, endNodeParent = lastAdjacentBeforeE!!)
+            .size
+    }
+
+    fun part2(chars2D: Array<CharArray>): Int {
+        val (_, eIndex) = chars2D.findEdgesIndices()
+
+        val graph = chars2D.drawHeightsGraph(onAdjacentFilter = { adjH, curH ->
+            chars2D[curH.y][curH.x] - chars2D[adjH.y][adjH.x] < 2
+        })
+
+        val queue = ArrayDeque(listOf(eIndex))
+        val searched = mutableSetOf<Ver>()
+        val parents = mutableMapOf<Ver, Ver>()
+
+        var lastAdjacentBeforeA: Ver? = null
+        var fastestFoundA: Ver? = null
+
+        while (queue.any()) {
+            val currPositionVer = queue.removeFirst()
+            if (currPositionVer in searched) continue
+            searched += currPositionVer
+
+            val adjacentVertices: Set<Ver> = graph[currPositionVer]!!
+
+            adjacentVertices
+                .filterNot { it in searched }
+                .forEach { parents[it] = currPositionVer }
+
+            if (adjacentVertices.any { chars2D[it.y][it.x] == 'a' }) {
+                lastAdjacentBeforeA = currPositionVer
+                fastestFoundA = adjacentVertices.single { chars2D[it.y][it.x] == 'a' }
+                break
+            } else
+                adjacentVertices.forEach(queue::addLast)
+        }
+
+        return parents
+            .buildWalkedPath(endNode = fastestFoundA!!, endNodeParent = lastAdjacentBeforeA!!)
+            .size
+    }
+
+
+    val testInput = readInputAs2DCharArray("Day12_test")
+    val input = readInputAs2DCharArray("Day12")
+
+
+    part1(testInput.cloneOf2D()).checkEquals(31)
+    part1(input.cloneOf2D())
+        .checkEquals(462)
+//        .sendAnswer(part = 1, day = "12", year = 2022)
+
+
+    part2(testInput.cloneOf2D()).checkEquals(29)
+    part2(input.cloneOf2D())
+        .checkEquals(451)
+//        .sendAnswer(part = 2, day = "12", year = 2022)
+
+}
--- src/Utils.kt
+++ src/Utils.kt
@@ -13,9 +13,12 @@ fun readInputAsText(name: String) = File("src/resources", "$name.txt")
 
 fun readInputAs2DIntArray(name: String) = readInput(name)
     .map { line -> line.map { it.digitToInt() }.toIntArray() }.toTypedArray()
+inline fun Array<IntArray>.cloneOf2D() = this.map { it.clone() }.toTypedArray()
 
-fun readInputAs2DIntList(name: String) = readInput(name)
-    .map { line -> line.map { it.digitToInt() } }
+fun readInputAs2DCharArray(name: String) = readInput(name)
+    .map { line -> line.toCharArray()}.toTypedArray()
+
+inline fun Array<CharArray>.cloneOf2D() = this.map { it.clone() }.toTypedArray()
 
 /**
  * Converts string to md5 hash.
@@ -23,3 +26,23 @@ fun readInputAs2DIntList(name: String) = readInput(name)
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
+
+inline fun <reified T: Any> T.alsoPrintln(): T = this.also(::println)
+
+fun String.checkEquals(expected: String): String = this.also { check(it == expected){"\nExpected: $expected\n Found: $this"} }
+
+inline fun <reified T: Number> T.checkEquals(expected: T): T = this.also { check(it == expected){"\nExpected: $expected\nFound: $this"} }
+//    sendResult(answer = p2.toString(), part = 2, day = "12", year = 2022)
+
+fun String.sendAnswer(part: Int, day:String, year: Int = 2022) = sendResult(
+    day = day,
+    part = part,
+    answer = this,
+    year = year
+)
+inline fun <reified T: Number> T.sendAnswer(part: Int, day:String, year: Int = 2022) = sendResult(
+    day = day,
+    part = part,
+    answer = this.toString(),
+    year = year
+)
